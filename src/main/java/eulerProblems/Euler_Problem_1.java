8e4b38b76187e6e0685b74397f902af923a369ed
cedarsphoto <33234670+cedarsphoto@users.noreply.github.com>
2020-02-13 11:24:51 +0000

    Fixed problems 1 & 2

--- src/main/java/eulerProblems/Euler_Problem_1.java
+++ src/main/java/eulerProblems/Euler_Problem_1.java
@@ -3,17 +3,23 @@ package eulerProblems;
 public class Euler_Problem_1 {
 
     public static void main(String[] args) {
+        // sets the limit for how high we want to go
         int cap = 1000;
+        // these two numbers are the key ones from the problem
         int num1 = 3;
         int num2 = 5;
+        // sets the running total to 0
         int output = 0;
         int i;
 
         for (i = 0; i < cap; i++) {
+            // checks if the current number in sequence is cleanly divisible by num1 and num2
             if (((i % num1) == 0) || ((i % num2) == 0)) {
-                output = output + 1;
+                output = output + i;
             }
         }
+
+        System.out.println(output);
     }
 
 }
--- src/main/java/eulerProblems/Euler_Problem_2.java
+++ src/main/java/eulerProblems/Euler_Problem_2.java
@@ -4,9 +4,9 @@ public class Euler_Problem_2 {
 
     public static void main(String[] args) {
         int i;
-        int output =0;
-        int seed1 = 0;
-        int seed2 = 0;
+        int output =2;
+        int seed1 = 1;
+        int seed2 = 2;
         int n=0;
 
         for (i=0;i<4000000;i++) {
